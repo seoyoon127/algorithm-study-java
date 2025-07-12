509144a8c5b3fc543b6b02b42b2684bc3fcaf5e3
seoyoon lee <leeseoyoon01@gmail.com>
2025-07-12 21:35:07 +0900

    day6 modify

--- src/silver/BOJ2559.java
+++ src/silver/BOJ2559.java
@@ -5,27 +5,23 @@ import java.util.Scanner;
 public class BOJ2559 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input1 = sc.nextLine();
-        int N = Integer.parseInt(input1.split(" ")[0]);
-        int T = Integer.parseInt(input1.split(" ")[1]);
-
+        int N = sc.nextInt();
+        int T = sc.nextInt();
 
         int[] arr = new int[N];
         for(int i=0; i<N; i++){
             arr[i] = sc.nextInt();
         }
 
-        long ans = 0;
-        for(int i=0; i<N-T+1; i++){
-            long x = 0;
-            for(int j=i; j<i+T; j++){
-                x += arr[j];
-            }
-            if(i==0)
-                ans = x;
-            else
-                ans = Math.max(ans, x);
+        long window = 0;
+        for (int i=0; i<T; i++) window+=arr[i];
+        long max = window;
+
+        for (int i=T; i<N; i++) {
+            window = window - arr[i-T] + arr[i];
+            if (window > max)
+                max = window;
         }
-        System.out.println(ans);
+        System.out.println(max);
     }
 }
