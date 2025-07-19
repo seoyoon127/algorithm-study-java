d825d0c4c22c41dad43e2d60ed2a00fed7be65a0
seoyoon lee <leeseoyoon01@gmail.com>
2025-07-19 22:59:38 +0900

    day11 modify

--- src/silver2/BOJ18870.java
+++ src/silver2/BOJ18870.java
@@ -1,56 +1,34 @@
 package silver2;
 
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
+
 
 public class BOJ18870 {
-    static int N;
-    static int[] sortArr;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
+        int N = sc.nextInt();
         int[] arr = new int[N];
-
-        Set<Integer> set = new LinkedHashSet<>();
         for (int i=0; i<N; i++){
             arr[i] = sc.nextInt();
-            set.add(arr[i]);
         }
 
-        sortArr = new int[set.size()];
-        int index = 0;
-        for (int s : set){
-            sortArr[index++] = s;
+        Set<Integer> set = new HashSet<>();
+        for (int a : arr){
+            set.add(a);
         }
-        Arrays.sort(sortArr);
+
+        List<Integer> sortArr = new ArrayList<>(set);
+        Collections.sort(sortArr);
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i<sortArr.size(); i++){
+            map.put(sortArr.get(i), i);
+        }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append(binary(arr[i])).append(" ");
+        for (int i=0; i<N; i++){
+            sb.append(map.get(arr[i])).append(" ");
         }
         System.out.println(sb);
-    }
-    static int binary(int a){
-        int st = 0;
-        int en = sortArr.length-1;
-        int result = 0;
-
-        while(st <= en){
-            int mid = (st + en)/2;
-
-            if (sortArr[mid] < a){
-                st = mid + 1;
-            }
-            else if (sortArr[mid] > a){
-                en = mid - 1;
-            }
-            else{
-                result = mid;
-                break;
-            }
-        }
-        return result;
     }
 }
