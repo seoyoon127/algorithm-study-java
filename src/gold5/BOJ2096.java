32848c9f3f70b8c0ffe73d410d1d33622d52fd14
seoyoon lee <leeseoyoon01@gmail.com>
2025-08-31 20:28:56 +0900

    day35 modify

--- src/gold5/BOJ2096.java
+++ src/gold5/BOJ2096.java
@@ -13,32 +13,36 @@ public class BOJ2096 {
                 arr[i][j] = sc.nextInt();
             }
         }
-        int[][] maxArr = new int[N][3];
-        int[][] minArr = new int[N][3];
-        maxArr[0] = arr[0].clone();
-        minArr[0] = arr[0].clone();
+        int[] maxArr = new int[3];
+        int[] minArr = new int[3];
+        maxArr = arr[0].clone();
+        minArr = arr[0].clone();
 
         for (int i=1; i<N; i++){
+            int[] currMax = new int[3];
+            int[] currMin = new int[3];
             for (int j=0; j<3; j++){
                 int max = Integer.MIN_VALUE;
                 int min = Integer.MAX_VALUE;
                 for (int k=0; k<3; k++){
                     int td = j + add[k];
                     if (td >=0 && td<3){
-                        max = Math.max(max, maxArr[i-1][td]);
-                        min = Math.min(min, minArr[i-1][td]);
+                        max = Math.max(max, maxArr[td]);
+                        min = Math.min(min, minArr[td]);
                     }
                 }
-                maxArr[i][j] = max + arr[i][j];
-                minArr[i][j] = min + arr[i][j];
+                currMax[j] = max + arr[i][j];
+                currMin[j] = min + arr[i][j];
             }
+            maxArr = currMax;
+            minArr = currMin;
         }
 
         int ansMax = Integer.MIN_VALUE;
         int ansMin = Integer.MAX_VALUE;
         for (int i=0; i<3; i++){
-            ansMax = Math.max(ansMax, maxArr[N-1][i]);
-            ansMin = Math.min(ansMin, minArr[N-1][i]);
+            ansMax = Math.max(ansMax, maxArr[i]);
+            ansMin = Math.min(ansMin, minArr[i]);
         }
         System.out.println(ansMax + " " + ansMin);
     }
